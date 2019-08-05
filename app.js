2b9ab149d0075087945026f852399e678a4368e1
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-08-05 14:52:18 -0700

    update

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, './app/index.html'));
 });
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./app/index.html"));
-});
-
 app.use(express.static("./app")); // set static files location, in this case the route, add a file name if not
-app.listen(process.env.PORT || 8000, function () {
+app.listen(PORT, function () {
   console.log("Listening on port:" + PORT);
 });
